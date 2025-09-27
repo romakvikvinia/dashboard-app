e1b446ba04a1315573c640e1b2cfc046d94547f1
Roma <romakvikvinia@gmail.com>
2025-09-27 21:35:07 +0400

    add typescript & home page

---
+++ site/src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+import Header from "./common/Header";
+import Sidebar from "./common/SideBar";
+import Home from "./modules/home/Home";
+
+import "./assets/scss/style.scss";
+
+function App() {
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false);
+
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle);
+  };
+
+  return (
+    <div className="grid-container">
+      <Header OpenSidebar={OpenSidebar} />
+      <Sidebar
+        openSidebarToggle={openSidebarToggle}
+        OpenSidebar={OpenSidebar}
+      />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
